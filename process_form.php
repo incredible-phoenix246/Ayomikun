edd7c3dca168324e8b95ee2552ee5a6ad0756b44
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-09-08 22:28:38 +0100

    new

---
+++ process_form.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+
+    $to = "ayomikuntemitope246@gmail.com";
+    $subject = "New Form Submission";
+    $headers = "From: $email";
+
+    // Compose the email message
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n";
+
+    // Send the email
+    mail($to, $subject, $email_message, $headers);
+
+    // Redirect to a thank-you page
+    header("Location: thank_you.html");
+}
+?>
