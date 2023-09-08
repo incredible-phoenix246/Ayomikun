a7c0883d020e480984ddfe0bfb0c798c9d5acb1a
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-09-08 22:47:57 +0100

    checking

--- process_form.php
+++ process_form.php
@@ -3,19 +3,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
 
-    $to = "ayomikuntemitope246@gmail.com";
-    $subject = "New Form Submission";
-    $headers = "From: $email";
+    // Create a filename for the storage file (you can customize this)
+    $filename = "form_data_" . date("Y-m-d_H-i-s") . ".txt";
 
-    // Compose the email message
-    $email_message = "Name: $name\n";
-    $email_message .= "Email: $email\n";
+    // Compose the data to be written to the file
+    $data = "Name: $name\n";
+    $data .= "Email: $email\n\n";
 
-    // Send the email
-    mail($to, $subject, $email_message, $headers);
+    // Specify the directory where you want to store the files
+    $storageDirectory = "form_data/";
 
-    // Redirect to a thank-you page
-    header("Location: thank_you.html");
-    exit;
+    // Check if the storage directory exists, and create it if not
+    if (!is_dir($storageDirectory)) {
+        mkdir($storageDirectory, 0755, true);
+    }
+
+    // Write the data to the file
+    if (file_put_contents($storageDirectory . $filename, $data) !== false) {
+        // Redirect to the thank you page
+        header("Location: thank_you.html");
+        exit;
+    } else {
+        // If writing to the file fails, you can handle the error here
+        echo "Error: Unable to store form data.";
+    }
 }
 ?>
